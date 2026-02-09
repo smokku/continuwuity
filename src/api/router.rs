c967712896b41fc346d0c482ec85c8d9beb672aa
Shane Jaroch <chown_tee@proton.me>
2026-02-09 17:17:50 -0500

    feat: notifications (cinny/schildichat)
    implement: configurable `notification_max_limit` and `notification_max_pdus_per_room`
    
    implement: a heap to stop memory leak at the cost of some CPU at message receive time
    
    other:
            add some monitoring/debugging outputs
            add warn log in 2nd potential breaking point

--- src/api/router.rs
+++ src/api/router.rs
@@ -30,6 +30,7 @@ pub fn build(router: Router<State>, server: &Server) -> Router<State> {
 		.ruma_route(&client::get_register_available_route)
 		.ruma_route(&client::register::register_route)
 		.ruma_route(&client::register::request_registration_token_via_email_route)
+		.ruma_route(&client::notifications::get_notifications_route)
 		.ruma_route(&client::get_login_types_route)
 		.ruma_route(&client::login_route)
 		.ruma_route(&client::login_token_route)
